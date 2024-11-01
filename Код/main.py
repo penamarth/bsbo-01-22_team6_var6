0715da13940d4e81649244c348d662b546ba72c9
sjewyr <ganshin.2004@gmail.com>
2024-11-01 17:23:38 +0300

    fix: bug

--- Код/main.py
+++ Код/main.py
@@ -6,9 +6,7 @@ from classes.operation import ShipmentOperation
 from classes.user import Operator
 
 ware = Warehouse(1,"123")
-
 rack = Rack(1, "123")
-
 ware.addStorageLocation(rack)
 rack.addShelf(Shelf(1, "blabla"))
 
